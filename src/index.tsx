6ef6b81ec494699fc873ec30cb2e659e0fd2c285
Cyboticx LLC <opokudavid446@gmail.com>
2022-09-03 23:59:50 +0000

    redirect fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/index.scss";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
 import Bootstrap from "./Bootstrap";
 
 const root = ReactDOM.createRoot(
